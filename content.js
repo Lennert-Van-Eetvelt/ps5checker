fc0c6ed06b9be7ea54afcad6cd5cc4cf21f7aef7
Lennert-Van-Eetvelt <lennert.vaneetvelt@gmail.com>
2022-01-10 20:58:01 +0000

    first version

--- content.js
+++ content.js
@@ -4,8 +4,8 @@ setTimeout(function (){main();},3000);
 // document.onload = function (){ console.log("loadd");main();};
 
 function goToUrl(url, timeout){
-    if (!active)
-        return;
+    // if (!active)
+    //     return;
     console.log("changing url to " + url)
     if (active)
         saveActive(active)
@@ -46,8 +46,10 @@ function main(){
         case 8: checkCoolblueAll(5,"https://www.coolblue.be/nl/product/895374");return;
         case 9: checkCoolblueAll(6,"https://www.coolblue.be/nl/product/895975");return;
         case 10: checkCoolblueAll(7,"https://www.coolblue.be/nl/product/894891");return;
-        case 10: checkAmazon();return;
-        case 11: place = 0; main(); return;
+        case 11: checkAmazonDigital();return;
+        // case 12: checkAmazonDisc();return;
+        case 12: checkNedGame();return;
+        case 13: place = 0; main(); return;
     }
 }
 
@@ -56,12 +58,15 @@ function found(website, url){
     active = false;
     saveActive(false)
     console.log("OMG I Found it ")
-    sendSMS()
+    sendMessage(website,url)
 }
 
 
-function sendSMS(){
-    //...
+function sendMessage(title, message){
+    var url = new URL("https://api.simplepush.io/send/46XqyU/"+title+"/")
+    url.searchParams.append('x',message)
+    console.log(url.toString().replace("?x=", ""))
+    goToUrl(url.toString().replace("?x=", "")+"/event/Critical",0)
 }
 
 
@@ -69,9 +74,9 @@ function checkBol(){
     console.log("checking bol")
     let urlBase = "https://www.bol.com/be/nl/p/sony-playstation-5-console/"
     let urlFull = "https://www.bol.com/be/nl/p/sony-playstation-5-console/9300000004162282/?ruleRedirect=1&sI=playstation%205&variants="
-    if (!window.location.href.startsWith(urlBase)) goToUrl(urlFull, 3898); else{
+    if (!window.location.href.startsWith(urlBase)) goToUrl(urlFull, 898); else{
     let ele = document.getElementsByClassName("buy-block__title")[0];
-    if (!ele.innerHTML.startsWith("Niet")) found("bol",urlFull);
+    if (!ele.innerText.startsWith("Niet")) found("bol",urlFull);
     else console.log("not found....")
     place++;
     savePlace();
@@ -80,9 +85,9 @@ function checkBol(){
 function checkAlternateDisc(){
     console.log("checking alternate disc")
     let urlFull = "https://www.alternate.be/Sony-Interactive-Entertainment/PlayStation-5-spelconsole/html/product/1651220"
-    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 3898); else{
+    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 898); else{
     let ele = document.getElementsByClassName("d-flex justify-content-center align-items-center")[0].children[0];
-    if (!ele.innerHTML.startsWith("Niet")) found("alternate", urlFull);
+    if (!ele.innerText.startsWith("Niet")) found("alternate", urlFull);
     else console.log("not found....")
     place++;
     savePlace();
@@ -91,9 +96,9 @@ function checkAlternateDisc(){
 function checkAlternateDigital(){
     console.log("checking alternate digital")
     let urlFull = "https://www.alternate.be/Sony-Interactive-Entertainment/PlayStation-5-Digital-Edition-spelconsole/html/product/1651221"
-    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 3898); else{   
+    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 898); else{   
     let ele = document.getElementsByClassName("d-flex justify-content-center align-items-center")[0].children[0];
-    if (!ele.innerHTML.startsWith("Niet")) found("alternate", urlFull);
+    if (!ele.innerText.startsWith("Niet")) found("alternate", urlFull);
     else console.log("not found....")
     place++;
     savePlace();
@@ -102,11 +107,11 @@ function checkAlternateDigital(){
 function checkCoolblueAll(number, url){
     console.log("checking cooblue "+number)
     let urlFull = url ;
-    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 3898); else{
+    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 898); else{
     let available = true;
     try {
         let ele = document.getElementsByClassName("js-threshold-toggle-sticky-bar")[0].getElementsByClassName("color--unavailable")[0];
-        if (ele.innerHTML.startsWith("Tijdelijk")) available = false;
+        if (ele.innerText.startsWith("Tijdelijk")) available = false;
     }catch (E){
         console.log(e)
     }
@@ -120,11 +125,13 @@ function checkCoolblueAll(number, url){
 function checkAmazonDigital(){
     console.log("checking Amazon digital")
     let urlFull = "https://www.amazon.de/-/en/dp/B08H98GVK8/";
-    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 3898); else{
+    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 898); else{
     let available = true;
     try {
-        let ele = document.getElementById("availability").children[0].children[0].children[0];
-        if (ele.innerHTML.startsWith("Currently")) available = false;
+        let ele = document.getElementById("availability").children[0];//.children[0];//.children[0];
+        console.log(ele)
+        console.log(ele.innerText)
+        if (ele.innerText.startsWith("Currently")) available = false;
     }catch (E){
         console.log(e)
     }
@@ -138,15 +145,33 @@ function checkAmazonDigital(){
 function checkAmazonDisc(){
     console.log("checking Amazon disc")
     let urlFull = "https://www.amazon.de/-/en/dp/B08H93ZRK9/";
-    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 3898); else{
+    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 898); else{
         let available = true;
         try {
-            let ele = document.getElementById("availability").children[0].children[0].children[0];
-            if (ele.innerHTML.startsWith("Currently")) available = false;
+            let ele = document.getElementById("availability").children[0];//.children[0].children[0];
+            if (ele.innerText.startsWith("Currently")) available = false;
         }catch (E){
             console.log(e)
         }
         if (available) found("amazon disc", urlFull)
+        else console.log("not found....")
+        place++;
+        savePlace();
+        main();}
+}
+
+function checkNedGame(){
+    console.log("checking nedgame")
+    let urlFull = "https://www.nedgame.nl/playstation-5/playstation-5-digital-edition-bundel/3831177925/";
+    if (!window.location.href.startsWith(urlFull)) goToUrl(urlFull, 898); else{
+        let available = true;
+        try {
+            let ele = document.getElementsByClassName("koopdiv")[0].children[0];//.children[0].children[0];
+            if (ele.innerText.startsWith("Uitverkocht")) available = false;
+        }catch (E){
+            console.log(e)
+        }
+        if (available) found("nedgame", urlFull)
         else console.log("not found....")
         place++;
         savePlace();
